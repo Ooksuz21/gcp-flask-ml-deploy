33f23c7dd024aba0d8400cfb80645c85ca886e23
Ooksuz21 <147621728+Ooksuz21@users.noreply.github.com>
2024-08-29 13:11:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return 'Hello World! selamun aleykum'
+    return 'Hello World! naber nigga'
 
 @app.route('/echo/<name>')
 def echo(name):
